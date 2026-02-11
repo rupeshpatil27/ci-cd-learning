c1780001f9c5c7e90aecd939ab6117ccad85eb96
rupeshpatil <rupeshpatil88927@gmail.com>
2026-02-11 23:58:01 +0530

    initial commit

---
+++ basic_workflow/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/add', (req, res) => {
+  const { a, b } = req.query;
+  const result = Number(a) + Number(b);
+  res.json({ result });
+});
+
+module.exports = app;
